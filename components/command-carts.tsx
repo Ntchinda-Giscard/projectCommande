83f4b634fd286e88fdf1e8d6a08b54c06e448d33
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-06-27 11:33:46 +0100

    command status badge

--- components/command-carts.tsx
+++ components/command-carts.tsx
@@ -4,11 +4,12 @@ import React from 'react'
 const CommandCarts = () => {
   return (
     <View className='flex border border-borders p-3 bg-white rounded-lg mt-4'>
-        <View>
+        <View className='flex flex-row  justify-beween items-center'>
             <View className='flex flex-row gap-2'>
-                <Text className='text-xl font-semibold'> Order #1750852523117</Text>
-                <Badge text={'validate'} />
+                <Text className='text-xl font-semibold'>Order #17508</Text>
+                <Badge text={'validate'}/>
             </View>
+            <Text className='text-xl font-semibold'> XAF200 </Text>
         </View>
       <Text>CommandCarts</Text>
 
@@ -22,10 +23,15 @@ export default CommandCarts
 const Badge = ({text, variant}: {text: string, variant?: string}) => {
     let className = ""
     let textColor = ""
-    if(variant === 'light' ) className = "bg-slate-200 rounded-full py-2 px-4 w-fit"
-    else if(variant === 'outline' ) className = "bg-slate-200 rounded-full py-2 px-4 w-fit"
-    else className = "bg-black rounded-full py-1 px-2 w-fit"; textColor = "text-white";
+    if(variant === 'light' ){ 
+        className = "bg-slate-200 py-1 px-2 rounded-full w-fit"
+        textColor = "text-black"}
+    else if(variant === 'outline' ) {
+        className = "bg-transparent py-1 px-2 rounded-full w-fit"
+        textColor = "text-black"}
+    else {className = "bg-black py-1 px-2 rounded-full w-fit" 
+        textColor = "text-white"}
 
     return <View className={className}>
-        <Text className={textColor}> {text}  </Text> </View>
+        <Text className={`${textColor} text-xs font-semibold`}> {text}  </Text> </View>
 }
