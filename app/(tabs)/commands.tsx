703f875d2f5d21f861f6291e92d156ae7fa6226e
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-06-27 11:00:06 +0100

    comandde stat cards

--- app/(tabs)/commands.tsx
+++ app/(tabs)/commands.tsx
@@ -1,12 +1,13 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
 import StatCard from '@/components/stat-card'
 
 const Commands = () => {
   return (
-    <View>
-      <Text>Commands</Text>
-      <StatCard />
+    <View className='flex-1 p-5'>
+      <ScrollView>
+        <StatCard />
+      </ScrollView>
     </View>
   )
 }
